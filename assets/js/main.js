29dafbda67630d47fe4c0f92e0e812ad698f8016
soumalya biswas <soumalyaabiswas2001@gmail.com>
2023-03-24 04:05:21 +0530

    m

--- assets/js/main.js
+++ assets/js/main.js
@@ -110,7 +110,14 @@ themeButton.addEventListener('click', () => {
 })
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () =>{
+    const header = document.getElementById('header')
+this.scrollY >= 50 ? header.classList.add('bg-header')
+                   : header.classList.remove('bg-header')
 
+}
+
+window.addEventListener('scroll', scrollHeader)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 
